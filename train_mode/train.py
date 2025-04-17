eca77bfe909a6d3e6b3878581aba82c0f2f79055
Dmitrii <73591672+bushuevda@users.noreply.github.com>
2025-04-17 23:29:41 +0300

    Update train.py

--- train_mode/train.py
+++ train_mode/train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO("yolov8n.pt") 
-    results = model.train(data="data.yaml", epochs = 200, imgsz=900, device = 0, conf = 0.25, optimizer = "SGD")
+    results = model.train(data="data.yaml", epochs = 200, imgsz=800, device = 0, optimizer = "SGD")
